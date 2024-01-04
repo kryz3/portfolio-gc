13878e271efff12f5f6e83ce2f29b0cdf67193e6
kryz3 <pjgirault23@gmail.com>
2024-01-04 16:09:44 +0100

    pour juju :)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,11 @@
 import { defineConfig } from 'astro/config';
+import node from "@astrojs/node";
+import solidJs from '@astrojs/solid-js';
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: "server",
+  adapter: vercel(),
+  integrations: [solidJs()],
+});
